ecd83cecc65491a4977259ca3f974bc572730278
psycho909 <psychosocial909@gmail.com>
2018-05-14 01:22:52 +0800

    123

---
+++ js/app.js
@@ -0,0 +1,95 @@
+var currentState=0;
+	var currentPage=$('#title-page');
+	var pages=[];
+	pages.push($('#title-page'));
+	$('.team-page').each(function(index,item){
+		pages.push($(item))
+	})
+	$('#nav-left').on('click',_.debounce(function(){
+		currentState--;
+		if(currentState < 0){
+			currentState=pages.length-1;
+			var nextPage=pages[currentState];
+			var currLeague=currentPage.attr('id');
+			var nextLeague=nextPage.attr('id')
+			var currCrest=$('#'+currLeague+'-crest');
+			var nextCrest=$('#'+nextLeague+'-crest');
+
+			$('.team-page').removeClass('offstage-right').addClass('offstage-left')
+			$('#crests-parent div').removeClass('offstage-right').addClass('offstage-left')
+
+			currentPage.removeClass('active').addClass('offstage-left')
+			nextPage.removeClass('offstage-left').addClass('active')
+
+			currCrest.removeClass('active').addClass('offstage-left')
+			nextCrest.removeClass('offstage-left').addClass('active')
+			
+			var widthToTranslate=$('#title-page-background').width()+getTeamBackgroundLeftovers();
+			$('#background-images').css('transform','translateX(-'+widthToTranslate+'px)')
+			console.log('[widthToTranslate]:'+widthToTranslate)
+		}else{
+			var nextPage=pages[currentState];
+			var currLeague=currentPage.attr('id');
+			var nextLeague=nextPage.attr('id')
+			var currCrest=$('#'+currLeague+'-crest');
+			var nextCrest=$('#'+nextLeague+'-crest');
+
+			currentPage.removeClass('active').addClass('offstage-right')
+			nextPage.removeClass('offstage-left').addClass('active')
+
+			currCrest.removeClass('active').addClass('offstage-right')
+			nextCrest.removeClass('offstage-left').addClass('active')
+			var widthToTranslate=$('#title-page-background').width()+getTeamBackgroundLeftovers();
+			if(currentState == 0){
+				$('#background-images').css('transform','translateX(0px)')
+			}else{
+				$('#background-images').css('transform','translateX(-'+widthToTranslate+'px)')
+			}
+			console.log('[widthToTranslate]:'+widthToTranslate)
+		}
+		currentPage=nextPage
+	},400))
+	$('#nav-right').on('click',_.debounce(function(){
+		currentState++;
+		if(currentState > pages.length-1){
+			currentState=0;
+			var nextPage=pages[currentState];
+			var currLeague=currentPage.attr('id')
+			var nextLeague=nextPage.attr('id')
+			var currCrest=$('#'+currLeague+'-crest');
+
+			currentPage.removeClass('active')
+			currCrest.removeClass('active')
+			$('.team-page').removeClass('offstage-left').addClass('offstage-right')
+			$('#title-page').removeClass('offstage-left').addClass('active')
+			$('#crests-parent div').removeClass('offstage-left').addClass('offstage-right')
+
+			$('#background-images').css('transform','translateX('+0+'px)')
+		}else{
+			var nextPage=pages[currentState];
+			var currLeague=currentPage.attr('id')
+			var nextLeague=nextPage.attr('id')
+			var currCrest=$('#'+currLeague+'-crest');
+			var nextCrest=$('#'+nextLeague+'-crest');
+
+			currentPage.removeClass('active')
+			currCrest.removeClass('active')
+
+			currentPage.addClass('offstage-left')
+			currCrest.addClass('offstage-left')
+			nextPage.removeClass('offstage-right').addClass('active')
+			nextCrest.removeClass('offstage-right').addClass('active')
+
+			var widthToTranslate=$('#title-page-background').width()+getTeamBackgroundLeftovers();
+			$('#background-images').css('transform','translateX(-'+widthToTranslate+'px)')
+			console.log('[widthToTranslate]:'+widthToTranslate)
+		}
+		currentPage=nextPage
+	},400))
+
+	function getTeamBackgroundLeftovers(){
+		var leftovers=$('#team-page-background').width()-$(window).width()
+		var move=((currentState-1)/pages.length)*leftovers;
+		console.log('[move]: '+move)
+		return move
+	}
---
+++ js/main.js
@@ -0,0 +1,11 @@
+require.config({
+    baseUrl: "./js",
+    paths: {
+        "jquery": "./lib/jquery-3.1.1.min",
+        "handlebars":"./lib/handlebars-v4.0.10",
+        "text":"./lib/text",
+    }
+});
+require(["jquery","handlebars"], function ($,Handlebars) {
+
+});
